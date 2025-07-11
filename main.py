aa7625a8b222fbeb1ae8a3646d479b2ee688acf1
Mohit123-hash <151940544+Mohit123-hash@users.noreply.github.com>
2025-07-11 14:25:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+# print("helloworld")
+# print("my name is mohit")
+
+# var1=30
+# print(var1)
+
+# var=input("enter your name")
+# print(f"your name is {var}")
+
+# user_name=input("enter your name") 
+# user_age=input("enter your age")
+# user_rollno=input("enter your rollno")
+# user_adult=input("greater than 18 is adult")
+# user_address=input("hamirpur")
+# print(f"your name is {user_name}")
+# print(f"your age is {user_age}")
+# print(f"your roll_no {user_rollno} ")
+# print(f"your address is{user_address}")
+# print(f"your are adult{user_adult}")
+# arithematic operator
+
+# a=10
+# b=3
+# add=a+b
+# sub=a-b
+# multi=a*b
+# div=a/b
+# percentage=a%b
+# print(f"add {a+b}")
+# print(f"sub {a-b}") 
+# print(f"multi {a*b}")
+# print(f"div {a/b}") 
+# print(f"percentage {a%b}")
+
