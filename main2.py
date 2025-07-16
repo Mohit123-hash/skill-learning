d08344689d6fd5ac7b741f067583f295c95ddb5c
Mohit123-hash <151940544+Mohit123-hash@users.noreply.github.com>
2025-07-16 14:04:19 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,40 @@
+# def my_function():
+#     print("This is my function")
+# my_function()
+# def my_function(fname):
+# 	print(fname + "saini")
+
+# my_function("tushar")
+# my_function("tushar2")
+# my_function("tushar3")
+
+# def my_function(fname, lname):
+#     print(fname + " " + lname)
+#     my_function("Mohit ", "Kumar")
+
+# def my_function(*students):
+#     print("topper of class is "+ students[2])
+# my_function("tushar", "mohit", "rohit")
+
+# def my_function(child3, child2, child1):
+#     print("the youngest child is "+ child3)
+# my_function(child1="tushar", child2="mohit", child3="rohit")
+
+# def my_function(**kid):
+#     print("the last name is " + kid["lname"])
+#     my_function(fname="tushar", lname="saini")
+
+
+numbers= [23, 5, 42, 12, 37, 1, 30, 16, 48, 55, 
+ 8, 19, 51, 26, 34, 44, 0, 29, 53, 39, 
+ 10, 31, 17, 57, 3, 25, 46, 22, 14, 49, 
+ 6, 41, 58, 20, 36, 9, 52, 7, 28, 45, 
+ 15, 43, 2, 24, 38, 11, 33, 50, 4, 59, 
+ 13, 35, 18, 21, 47, 27, 32, 40, 54, 56]
+length = len(numbers)
+print(f"The length of the list is {length}")
+index=length- 1
+print(f"index value of list is {index}")
+
+
+
